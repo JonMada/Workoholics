b9a3ecfbeef1ac8571d384211d43d529275a2e6b
JonMadariaga <jonmadariaga93@gmail.com>
2025-02-19 20:28:01 +0100

    Text

---
+++ src/components/text.jsx
@@ -0,0 +1,25 @@
+const Text = () => {
+  return (
+    <section className="text-wrapper">
+      <p>
+        <div className="text-heading">
+          <span className="bullet">•</span>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam&nbsp;
+            <span className="bold">vulputate dui efficitur</span> ultrices
+            pharetra. Praesent tempor mollis purus nec fringilla.
+          </span>
+        </div>
+
+        <div className="text-content">
+          <span>
+            Vivamus ultrices est id ex congue, vel commodo lacus viverra.
+            Praesent congue ut sapien at aliquam.
+          </span>
+        </div>
+      </p>
+    </section>
+  );
+};
+
+export default Text;
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero-section";
+import Text from "../components/text";
 
 const LandingPage = () => {
   return (
     <div className="landing-page-wrapper">
       <Hero />
+      <Text />
     </div>
   );
 };
